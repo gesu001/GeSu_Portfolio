3f5bb6fccc6a5568ca3984b3be9bf2e63b4d0103
Ge Su <gegege.su@gmail.com>
2023-08-05 10:08:46 +1000

    added contact.js and about me.js and utils file

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -5,7 +5,7 @@ export default function About() {
     <div>
       <h1>About Me</h1>
       <p>
-        This is me
+        Hi, my name is Ge Su. I am a IT student studying in Deakin University. I also study Full-Stack web developer coding bootcamp at University of Adelaide. I have great passion about technology and coding. In my spare time, I like to go to gym and learn belly dancing. I enjoy going out with friends for hiking during the weekends. 
       </p>
     </div>
   );
--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -1,12 +1,84 @@
-import React from 'react';
+import React, { useState } from 'react';
+import './style.css';
+
+// Here we import a helper function that will check if the email is valid
+import { validateEmail } from '../utils/helpers';
 
 export default function Contact() {
+  // Create state variables for the fields in the form
+  // We are also setting their initial values to an empty string
+  const [email, setEmail] = useState('');
+  const [userName, setUserName] = useState('');
+  const [text, setText] = useState('');
+  const [errorMessage, setErrorMessage] = useState('');
+
+  const handleInputChange = (e) => {
+    // Getting the value and name of the input which triggered the change
+    const { target } = e;
+    const inputType = target.name;
+    const inputValue = target.value;
+
+    // Based on the input type, we set the state of either email, username, and password
+    if (inputType === 'email') {
+      setEmail(inputValue);
+    } else if (inputType === 'userName') {
+      setUserName(inputValue);
+    } else {
+      setText(inputValue);
+    }
+  };
+
+  const handleFormSubmit = (e) => {
+    // Preventing the default behavior of the form submit (which is to refresh the page)
+    e.preventDefault();
+
+    // First we check to see if the email is not valid or if the userName is empty. If so we set an error message to be displayed on the page.
+    if (!validateEmail(email) || !userName) {
+      setErrorMessage('Email or username is invalid');
+      // We want to exit out of this code block if something is wrong so that the user can correct it
+      return;
+    }
+    // If everything goes according to plan, we want to clear out the input after a successful registration.
+    setUserName('');
+    setText('');
+    setEmail('');
+  };
+
   return (
     <div>
-      <h1>Contact Page</h1>
-      <p>
-        Welcome to contact me
-      </p>
+      <h2>Contact</h2>
+      <form className="form">
+        <label for="userName" className="form-label">Name</label>
+        <input
+          value={userName}
+          name="userName"
+          onChange={handleInputChange}
+          type="text"
+          placeholder="Your Name"
+        />
+        <label for="email" className="form-label">Email</label>
+        <input
+          value={email}
+          name="email"
+          onChange={handleInputChange}
+          type="email"
+          placeholder="Email"
+        />
+        <label for="textarea" className="form-label">Message</label>
+        <textarea
+          value={text}
+          name="text"
+          onChange={handleInputChange}
+          type="text"
+          placeholder='Message'
+        />
+        <button type="button" onClick={handleFormSubmit}>Submit</button>
+      </form>
+      {errorMessage && (
+        <div>
+          <p className="error-text">{errorMessage}</p>
+        </div>
+      )}
     </div>
   );
 }
