3c083413f96045eb4503e9a9bba39cb524cbba51
Ge Su <gegege.su@gmail.com>
2023-08-05 10:21:59 +1000

    added resume.js

--- src/components/pages/Resume.js
+++ src/components/pages/Resume.js
@@ -3,10 +3,28 @@ import React from 'react';
 export default function Resume() {
   return (
     <div>
-      <h1>My Resume</h1>
-      <p>
-        My Resume
-      </p>
+      <h1>Resume</h1>
+      {/* <p>Down load my <a href>Resume</a></p> */}
+      <h2>Front-end Proficienies</h2>
+      <ul>
+        <li>HTML</li>
+        <li>CSS</li>
+        <li>Bootstrap</li>
+        <li>JavaScript</li>
+        <li>JQuery</li>
+        <li>Responsive Design</li>
+        <li>React</li>
+      </ul>
+      <h2>Back-end Proficienies</h2>
+      <ul>
+        <li>APIs</li>
+        <li>Node</li>
+        <li>Express</li>
+        <li>MySQL Sequelize</li>
+        <li>MongoDB, Moongoose</li>
+        <li>REST</li>
+        <li>GraphQL</li>
+      </ul>
     </div>
   );
 }
